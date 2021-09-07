6798d7d622f6be8cf28c1bad0b726b4702c9e04c
Marcell Kajati <marcell.kajati@gmail.com>
2021-09-07 17:00:46 +0200

    basic model setup

---
+++ recipe-backend/Models/NutritionInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class NutritionInfo
+    {
+        public double Carbohydrates { get; set; }
+        public double Protein { get; set; }
+        public double Fats { get; set; }
+
+        public int RecipeId { get; set; }
+        public Recipe Recipe { get; set; }
+    }
+}
---
+++ recipe-backend/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(60)]
+        public string Name { get; set; }
+        [Required, MaxLength(500)]
+        public string Description { get; set; }
+        public double Calories { get; set; }
+        public int Servings { get; set; }
+        public NutritionInfo NutritionInfo { get; set; }
+        public long Duration { get; set; }
+    }
+}
